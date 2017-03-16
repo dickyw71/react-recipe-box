00e5f7de137fb207af170daeb40cd687a2dbd206
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-03-16 20:57:57 +0000

    Switched App component for RecipeBox and added initial data.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import RecipeBox from './RecipeBox.js';
+//import './index.css';
+
+
+var myRecipes = [{
+      title: "Jam Sandwich",
+      ingredients: ["White sliced bread", "Jam"]
+    }, {
+      title: "Chocolate Brownies",
+      ingredients: ["3 Eggs", "250g Plain Flour", "2tbsp Bicarbinate of soda", "800g Cocca powder"]
+}];
 
 ReactDOM.render(
-  <App />,
+  <RecipeBox recipes={myRecipes} />,
   document.getElementById('root')
 );
