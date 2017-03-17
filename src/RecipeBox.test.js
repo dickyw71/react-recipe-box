d4a0d0aa37dba96fefb8cee339bc784d082f669b
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-03-17 17:22:08 +0000

    Comments blocks added.

--- src/RecipeBox.test.js
+++ src/RecipeBox.test.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import RecipeBox from './RecipeBox.js';
 
+/** 
+ * Test data
+ */
 var myRecipes = [{
       title: "Jam Sandwich",
       ingredients: ["White sliced bread", "Jam"]
@@ -10,7 +13,12 @@ var myRecipes = [{
       ingredients: ["3 Eggs", "250g Plain Flour", "2tbsp Bicarbinate of soda", "800g Cocca powder"]
 }];
 
+/**
+ * RecipeBox tests
+ */
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<RecipeBox recipes={myRecipes} />, div);
 });
+
+
