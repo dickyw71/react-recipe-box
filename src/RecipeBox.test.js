134aaea2cc75b522bb0b8f40965e3a23606eaf29
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-03-17 08:35:21 +0000

    First commit of RecipeBox tests.

---
+++ src/RecipeBox.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RecipeBox from './RecipeBox.js';
+
+var myRecipes = [{
+      title: "Jam Sandwich",
+      ingredients: ["White sliced bread", "Jam"]
+    }, {
+      title: "Chocolate Brownies",
+      ingredients: ["3 Eggs", "250g Plain Flour", "2tbsp Bicarbinate of soda", "800g Cocca powder"]
+}];
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<RecipeBox recipes={myRecipes} />, div);
+});
